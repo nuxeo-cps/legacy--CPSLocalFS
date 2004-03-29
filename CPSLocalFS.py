4e5c6e2291170825186e164beb74a34b335acfe0
sastier <devnull@localhost>
2004-03-29 09:20:26 +0000

    Method getIconPath added

--- CPSLocalFS.py
+++ CPSLocalFS.py
@@ -1,17 +1,17 @@
 """Local File System product
 
 $Id$"""
-__version__='$Revision$'[11:-2]
+__version__='$Revision$'
 
-from zLOG import LOG, INFO, DEBUG
+
 from Products.CMFCore.CMFCorePermissions import View, ModifyPortalContent
 from Products.CPSCore.CPSBase import CPSBaseDocument, CPSBase_adder
 from Products.CMFCore import CMFCorePermissions
 from Products.LocalFS.LocalFS import LocalFS
 from Products.CPSDefault.Folder import Folder
 from AccessControl import ClassSecurityInfo
+from Products.PortalTransforms.MimeTypesRegistry import MimeTypesRegistry
 
-LOG("*sas* ===> ",INFO,"CPSLocalFS: Definition de la factory_type_information")
 
 factory_type_information = ({
       'id': 'CPSLocalFS',
@@ -39,23 +39,21 @@ factory_type_information = ({
   )
 
 class CPSLocalFS(LocalFS, Folder):
-    """Object that creates Zope objects from files in the local file system."""
+    """Object that creates Zope objects from files in the
+       local file system."""
     
     meta_type = 'CPS LocalFS'
     portal_type = meta_type
-   # _type_map = [] # TODO
+    # _type_map = [] # TODO
     _properties = LocalFS._properties
 
     security = ClassSecurityInfo()
 
-   
-
-    def __init__(self, id, **kw):                
+    def __init__(self, id, **kw):
         Folder.__init__(self, id, **kw)
-        XXX_default_path = '/home/sastier/tmp/'
-        LocalFS.__init__(self, id, XXX_default_path, None, None)
-        LOG("*sas* ===> ", INFO, "CPSlocalFS : fin de __init__")
-
+        basepath = kw.get('basepath','/home/sastier/tmp')
+        LocalFS.__init__(self, id, basepath, None, None)
+     
     security.declareProtected(ModifyPortalContent, 'edit')
     def edit(self, *args, **kw):
         print args
@@ -63,13 +61,22 @@ class CPSLocalFS(LocalFS, Folder):
         
     security.declareProtected(ModifyPortalContent, 'editProperties')
     def editProperties(self, title='', description='', basepath=''):
-        """Edit chat object properties.
-        """
-        self.manage_changeProperties(title=title, description=description, basepath=basepath)
+        """ Edit chat object properties."""
+        self.manage_changeProperties(title=title, description=description,\
+                                     basepath=basepath)
+        
+    security.declareProtected(ModifyPortalContent, 'getIconPath')
+    def getIconPath(self, type):
+        """ Return the icon registered fo a given type."""
+        types_registry = MimeTypesRegistry()
+        mimetypes = types_registry.lookup(type)
+        for type in mimetypes:
+            return type.icon_path
+        return atype
         
 
 def addCPSLocalFS(self, id, **kw):
-    #, basepath, username=None, password=None, REQUEST=None):
+       #, basepath, username=None, password=None, REQUEST=None):
     """Add a local file system object to a folder
   
     In addition to the standard Zope object-creation arguments,
@@ -79,16 +86,7 @@ def addCPSLocalFS(self, id, **kw):
         username -- Username for a network share (win32 only).
         password -- Password for a network share (win32 only).
     """
-
-    title = kw.get('title','Default Title')
-    basepath = kw.get('basepath', '/home/sastier/tmp')
-    
-    
-    LOG("*sas* ===> ",INFO,"CPSLocalFS: addCPSLocalFS()")
-    LOG("*sas* ===> Title: ",INFO,title)
-    LOG("*sas* ===> Basepath: ",INFO,basepath)
-    ob = CPSLocalFS(id, **kw)
-    LOG("*sas* ===> ",INFO,"avant CPSBase_adder")
+    ob = CPSLocalFS(id, **kw)   
     CPSBase_adder(self, ob)
-    LOG("*sas* ===> ",INFO,"apres CPSBase_adder")
-
+   
+ 
