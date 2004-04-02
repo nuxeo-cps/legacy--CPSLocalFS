2b52aa3da79bcc0e1c105a0defedd5c02e39d7e6
sastier <devnull@localhost>
2004-04-02 16:47:32 +0000

    A few name have been changed.

--- skins/cps_localfs/cpslocalfs_edit.py
+++ skins/cps_localfs/cpslocalfs_edit.py
@@ -18,19 +18,19 @@ try:
     fs.editProperties(title, basepath, description)
 
 except OSError:
-    REQUEST.set('portal_status_message','cpslocalfs_unknown_basepath_message')
+    REQUEST.set('portal_status_message','stm_cpslocalfs_unknown_basepath_message')
     return context.cpslocalfs_edit_form(context, REQUEST)
 
 except TypeError:
-    REQUEST.set('portal_status_message','cpslocalfs_insufficients_rights_message')
+    REQUEST.set('portal_status_message','stm_cpslocalfs_insufficients_rights_message')
     return context.cpslocalfs_edit_form(context, REQUEST)
 
 except AttributeError:
-    REQUEST.set('portal_status_message','cpslocalfs_invalid_content_in_folder_message')
+    REQUEST.set('portal_status_message','stm_cpslocalfs_invalid_content_in_folder_message')
     return context.cpslocalfs_edit_form(context, REQUEST)
 
 except NameError:
-    REQUEST.set('portal_status_message','cpslocalfs_invalid_basepath_message')
+    REQUEST.set('portal_status_message','stm_cpslocalfs_invalid_basepath_message')
     return context.cpslocalfs_edit_form(context, REQUEST)
 
 
