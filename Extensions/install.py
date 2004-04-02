92e1b16487f68fd8bef0d44193e79f9886ac0dc2
sastier <devnull@localhost>
2004-04-02 13:22:46 +0000

    Added method to supress action_status_history

--- Extensions/install.py
+++ Extensions/install.py
@@ -35,6 +35,8 @@ HOWTO USE THAT ?
 """
 
 from Products.CPSInstaller.CPSInstaller import CPSInstaller
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.Expression import Expression
 
 class ProductInstaller(CPSInstaller):
     product_name = 'CPSLocalFS'
@@ -53,8 +55,8 @@ class ProductInstaller(CPSInstaller):
         
         # Portal Workflow.
         self.allowContentTypes('CPSLocalFS', ('Workspace', 'Section'))
-        ws_chains = {'CPSLocalFS': 'workspace_content_wf',}
-        se_chains = {'CPSLocalFS': 'workspace_content_wf',}
+        ws_chains = {'CPSLocalFS': 'workspace_folder_wf',}
+        se_chains = {'CPSLocalFS': 'workspace_folder_wf',}
         self.verifyLocalWorkflowChains(self.portal['workspaces'],
                                         ws_chains)
         self.verifyLocalWorkflowChains(self.portal['sections'],
@@ -63,29 +65,50 @@ class ProductInstaller(CPSInstaller):
         # Portal Types.
         lfs_types = self.portal.getCPSLocalFSDocumentTypes()
         self.verifyFlexibleTypes(lfs_types)
-        self.log(str(lfs_types))
-
+        #self.log(str(lfs_types))
+        self.allowContentTypes('CPSLocalFS', ('Workspace', 'Section'))
+        ptypes = {
+            'CPSLocalFS' : {
+                'allowed_content_types': (),
+                'typeinfo_name': 'CPSLocalFS: CPSLocalFS',
+                'add_meta_type': 'Factory-based Type Information',
+             },
+        }
+        self.verifyContentTypes(ptypes)
         # Portal Schemas.
         lfs_schemas = self.portal.getCPSLocalFSDocumentSchemas()
         self.verifySchemas(lfs_schemas)
-        self.log(str(lfs_schemas))
+        #self.log(str(lfs_schemas))
 
         # Portal Layouts.
         lfs_layouts = self.portal.getCPSLocalFSDocumentLayouts()
         self.verifyLayouts(lfs_layouts)
-        self.log(str(lfs_layouts))
+        #self.log(str(lfs_layouts))
 
+        # Portal Actions :
+        #self.updateActions()
+        
         self.log("End of CPSLocalFS install.")
         self.finalize()
-
-
+        
     def updateCPS(self):
         """Update CPS
         """
         return self.portal.cpsupdate(self.portal)
-
-
     
+    def updateActions(self):
+        """ Remove action_status_history, should not be done in \
+        CPSLocalFS but in the product using it.
+        """
+        wftool = getToolByName(self.portal,'portal_actions')
+        self.log("les actions du portail : ")
+        for action in wftool._actions:
+            if action.Title() == 'action_status_history':
+                action.condition = Expression("python:getattr(object, \
+                'portal_type', None) not in ('CPSLocalFS','Section', \
+                'Workspace', 'Portal', 'Calendar', 'Event')")
+
+
 def install(self):
     installer = ProductInstaller(self)
     installer.install()
