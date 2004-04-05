31d03531070743207f6cf03d9231f80abce42495
sastier <devnull@localhost>
2004-04-05 13:08:37 +0000

    Keeps a backup of the attributs and re-modify CPSLocalFS in case the modification with the provided attributs failed

--- skins/cps_localfs/cpslocalfs_edit.py
+++ skins/cps_localfs/cpslocalfs_edit.py
@@ -1,4 +1,4 @@
-##parameters=title='', basepath='', description='', REQUEST=None
+##parameters=title='', path='', description='', REQUEST=None
 # $Id$
 
 """
@@ -13,26 +13,41 @@ conversion = {
 }
 
 
+
+fs = context.getContent()
+bck_title = fs.getTitle()
+bck_path = fs.getPath()
+bck_description = fs.getDescription()
+
 try:
-    fs = context.getContent()
-    fs.editProperties(title, basepath, description)
+    fs.editProperties(title, path, description)
 
 except OSError:
+    fs.editProperties(bck_title, bck_path, bck_description)
     REQUEST.set('portal_status_message','stm_cpslocalfs_unknown_basepath_message')
     return context.cpslocalfs_edit_form(context, REQUEST)
 
 except TypeError:
+    fs.editProperties(bck_title, bck_path, bck_description)
     REQUEST.set('portal_status_message','stm_cpslocalfs_insufficients_rights_message')
     return context.cpslocalfs_edit_form(context, REQUEST)
 
 except AttributeError:
+    fs.editProperties(bck_title, bck_path, bck_description)
     REQUEST.set('portal_status_message','stm_cpslocalfs_invalid_content_in_folder_message')
     return context.cpslocalfs_edit_form(context, REQUEST)
 
 except NameError:
+    fs.editProperties(bck_title, bck_path, bck_description)
     REQUEST.set('portal_status_message','stm_cpslocalfs_invalid_basepath_message')
     return context.cpslocalfs_edit_form(context, REQUEST)
 
 
+
+
 context.REQUEST.RESPONSE.redirect('./cpslocalfs_folder_contents')
+    
+
+
+
 
