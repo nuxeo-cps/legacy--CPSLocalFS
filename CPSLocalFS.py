863e8b9d67a8d5ff51785b686dec44287f16a3af
sastier <devnull@localhost>
2004-04-05 13:04:58 +0000

    Correction on basepath bug

--- CPSLocalFS.py
+++ CPSLocalFS.py
@@ -6,21 +6,19 @@ __version__='$Revision$'
 
 from Products.CMFCore.CMFCorePermissions import View, ModifyPortalContent
 from Products.CPSCore.CPSBase import CPSBase_adder
-from Products.CPSDocument.CPSDocument import CPSDocument as BaseDocument
-
-
 from Products.CMFCore import CMFCorePermissions
 from Products.LocalFS.LocalFS import LocalFS
 from Products.CPSDefault.Folder import Folder
 from AccessControl import ClassSecurityInfo
 from Products.PortalTransforms.MimeTypesRegistry import MimeTypesRegistry
 from Globals import InitializeClass
-
+#from zLOG import LOG, DEBUG, INFO
 
     
 factory_type_information = ({
       'id': 'CPS Local File System',
       'title': 'portal_type_CPSLocalFS_title',
+      'description': 'portal_type_CPSLocalFS_description',
       'icon': 'localfs_icon.gif',
       'product': 'CPSLocalFS',
       'meta_type': 'CPS Document',
@@ -45,48 +43,43 @@ factory_type_information = ({
       },
   )
 
-class CPSLocalFS(LocalFS, BaseDocument):
+
+
+
+class CPSLocalFS(LocalFS, Folder):
     """Object that creates Zope objects from files in the
        local file system."""
-   
     meta_type = 'CPSLocalFS'
     portal_type = meta_type
-    # _type_map = [] # TODO
     _properties = LocalFS._properties
-
-    
     security = ClassSecurityInfo()
 
-    def __init__(self, id, **kw):
-        BaseDocument.__init__(self, id, **kw)
-        self.datamodel = kw.get('datamodel')
-        self.lfs_title = self.datamodel['Title']
-        self.lfs_basepath = self.datamodel['Basepath']
-        self.lfs_description = self.datamodel['Description']
-        LocalFS.__init__(self, self.lfs_title, self.lfs_basepath, None, None)
-    
-            
-    #    Folder.__init__(self, id, **kw)
 
-     
+    def __init__(self, id, **kw):
+        Folder.__init__(self, id, **kw)
+        datamodel = kw.get('datamodel')
+        self.setTitle(datamodel['Title'])
+        # Warning : mother class LocalFS own a 'basepath'
+        # attribut that must not be override
+        self.setPath(datamodel['Basepath'])
+        self.setDescription(datamodel['Description'])
+        LocalFS.__init__(self, self.getTitle(), self.getPath(), None, None)
+
+        
     security.declareProtected(ModifyPortalContent, 'edit')
     def edit(self, *args, **kw):
         print args
         print kw
+
                 
     security.declareProtected(ModifyPortalContent, 'editProperties')
-    def editProperties(self, title, basepath, description):
+    def editProperties(self, title, path, description):
         """ Edit CPSLocalFS object properties."""
-        self.lfs_description = description
-        self.lfs_title = title
-        self.lfs_basepath = basepath
-
-##        LOG("* editLFS * datamodel ==> ",INFO,self.datamodel)
-##        LOG("* editLFS * title ==> ",INFO,self.lfs_title)
-##        LOG("* editLFS * descrip ==>", INFO,self.lfs_description)
-##        LOG("* editLFS * basepath ==>", INFO,self.lfs_basepath)
+        self.setPath(path)
+        self.setDescription(description)
+        self.setTitle(title)
         self.manage_changeProperties(title=title, description=description,\
-                                    basepath=basepath)
+                                    basepath=path)
         
     security.declareProtected(ModifyPortalContent, 'getIconPath')
     def getIconPath(self, type):
@@ -95,8 +88,49 @@ class CPSLocalFS(LocalFS, BaseDocument):
         mimetypes = types_registry.lookup(type)
         for type in mimetypes:
             return type.icon_path
-       
 
+
+    #
+    # Getters and Setters
+    #
+
+    #--
+    def getPath(self):
+        return self.path
+
+    def setPath(self, a_path):
+        self.path = a_path
+
+    #--
+    def getDescription(self):
+        return self.description
+
+    def setDescription(self, a_description):
+        self.description = a_description
+
+    #--
+    def getTitle(self):
+        return self.title
+
+    def setTitle(self, a_titre):
+        self.title = a_titre
+
+    #
+    # Helpers
+    #
+
+    #--
+    def toString(self):
+        """ Returns a string of the instance attributes title,
+            path and description. """
+        st = " \n"
+        st += "Title: "+self.getTitle()+"\n"
+        st += "Path: "+self.getPath()+"\n"
+        st += "Description: "+self.getDescription()+"\n"
+        return st
+
+
+        
 InitializeClass(CPSLocalFS)
 
 def addCPSLocalFS(container, id, **kw):
