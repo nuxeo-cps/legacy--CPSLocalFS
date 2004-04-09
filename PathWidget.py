eddf753087e94ce2802a91864c923424ff5e6162
sastier <devnull@localhost>
2004-04-09 13:11:49 +0000

    Path Widget now validate by checking if the path input is :  1. An existing path  2. A directory  3. A directory on which zopes has the correct access rights.

--- PathWidget.py
+++ PathWidget.py
@@ -3,76 +3,53 @@ from Products.CPSSchemas.BasicWidgets import CPSStringWidget, CPSStringWidgetTyp
 from Globals import InitializeClass
 from Products.CPSSchemas.WidgetTypesTool import WidgetTypeRegistry
 from Products.CPSSchemas.BasicWidgets import _isinstance, renderHtmlTag
-
 from Products.LocalFS.LocalFS import LocalFS
 from Products.CPSLocalFS.CPSLocalFS import CPSLocalFS
-from cgi import escape
 from Globals import InitializeClass
-from zLOG import LOG, DEBUG, INFO
+#from zLOG import LOG, DEBUG, INFO
+from os.path import exists, isdir
+from os import access, W_OK
 
 
 class PathWidget(CPSStringWidget):
-    """Function String widget.
-
-       String by calling the method specified in the _getFunction Method."""
+    """ Path Widget class, used to get the user inputs in order to create
+        an instance of CPSLocalFS. Its main purpose it to validate the
+        basepath input."""
 
     meta_type = "Path Widget"
     field_types = ('CPS String Field',)
-    
     display_width = 20
     size_max = 100
     _properties = CPSStringWidget._properties
     
     def prepare(self, datastructure, **kw):
         """Prepare datastructure from datamodel."""
-        LOG("*** PathWidget ***", INFO, " Prepare OK")
         datamodel = datastructure.getDataModel()
         datastructure[self.getWidgetId()] = datamodel[self.fields[0]]
-        #pass
-
+     
 
     def validate(self, datastructure, **kw):
         """Update datamodel from user data in datastructure."""
-        LOG("*** PathWidget ***", INFO, "validate(datastructure)")
         widget_id = self.getWidgetId()
         datamodel = datastructure.getDataModel()
         field_id = self.fields[0]
-        value = datastructure.get(widget_id,'')
-        LOG("path: ",INFO,value)
-        ok = 1
-        #a = CPSLocalFS(self, Title='tmptitlelfs', lfsbasepath=value,Description='')
-        try:
-            LOG("Try de LFS avec path : ",INFO,value)
-            a = LocalFS('tmptitlelfs', value, None, None)
-            LOG("______Fin du Try_______",INFO," ")
-            
-        except OSError:
-            LOG("PathWidget: ", INFO, "Unknown_basepath")
-            datastructure.setError(widget_id,"Unknown base path")
-            ok = 0
-            
-        except TypeError:
-            LOG("PathWidget: ", INFO, "insufficient_rights_messages")
-            datastructure.setError(widget_id,"Unknown base path")
-            ok = 0
-
-        except AttributeError:
-            LOG("PathWidget: ", INFO, "invalid_content")
-            datastructure.setError(widget_id,"Unknown base path")
-            ok = 0
-
-        except NameError:
-            LOG("PathWidget: ", INFO, "Invalid_Path")
-            datastructure.setError(widget_id,"Unknown base path")
-            ok = 0
-            
-            
-        except Exception, exception:
-            LOG("Uncacthed exception: ", INFO, exception.args)
-            datastructure.setError(widget_id,"Uncatch exception: "+str(exception.args))
-            ok = 0
-
-        datamodel.set(field_id,value)
+        a_path = datastructure.get(widget_id,'')
+        
+        ok = 0
+        if not exists(a_path):
+            #LOG("PathWidget: ", DEBUG, "Path doesn't exist")
+            datastructure.setError(widget_id,"psm_cpslocalfs_invalid_basepath_message")
+        else:
+            if not isdir(a_path):
+                #LOG("PathWidget: ", DEBUG, "Path is not a directory")
+                datastructure.setError(widget_id,"psm_cpslocalfs_unknown_basepath_message")
+            else:
+                if not access(a_path,W_OK):
+                    #LOG("PathWidget: ", DEBUG, "Insufficient Rights")
+                    datastructure.setError(widget_id,"psm_cpslocalfs_insufficients_rights_message")
+                else:
+                    ok = 1
+                    datamodel.set(field_id,a_path)
         return ok
 
     
@@ -82,21 +59,18 @@ class PathWidget(CPSStringWidget):
     
     def render(self, mode, datastructure, **kw):
         """Render this widget from the datastructure or datamodel."""
-        LOG("*** PathWidget ***", INFO, " Render OK")
         value = datastructure[self.getWidgetId()]
         
         if mode in ['view','edit','create']:
-            # XXX TODO should use an other name than kw !
-            # XXX change this everywhere
-            kw = {
+            desc = {
                   'id'  : self.getHtmlWidgetId(),
                   'name': self.getHtmlWidgetId(),
                   'value': value,
                   'size': self.display_width,
                   }
             if self.size_max:
-                kw['maxlength'] = self.size_max
-            return renderHtmlTag('input', **kw) + escape(value)
+                desc['maxlength'] = self.size_max
+            return renderHtmlTag('input', **desc)
     
 
 
