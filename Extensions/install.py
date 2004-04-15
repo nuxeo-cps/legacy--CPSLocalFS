0d783b9548c11ff8723a13e0abce3a0e6071c462
sastier <devnull@localhost>
2004-04-15 16:30:11 +0000

    Tests added to check if the right version of LocalFS is installed and then to check if the configuration file 'localfs_dirs.txt' can be acceded in HOME_ZOPE/var/

--- Extensions/install.py
+++ Extensions/install.py
@@ -21,7 +21,8 @@
 CPSLocalFS Installer
 
 HOWTO USE THAT ?
- - Make sure you have LocalFS-1-1-0 in your Products field.
+ - Make sure you have the right version of LocalFS in your
+   'Products' field.
  - Log into the ZMI as manager
  - Go to your CPS root directory
  - Create an External Method with the following parameters:
@@ -36,24 +37,37 @@ HOWTO USE THAT ?
 
 from Products.CPSInstaller.CPSInstaller import CPSInstaller
 from Products.CMFCore.utils import getToolByName
+from os.path import exists, isdir
+from zLOG import LOG, INFO, DEBUG
 
 
 class ProductInstaller(CPSInstaller):
+    """ Installer for CPSLocalFS Product. """
     product_name = 'CPSLocalFS'
-
     SKINS = {'cps_localfs': 'Products/CPSLocalFS/skins/cps_localfs'}
-
+    
     def install(self):
         self.log("Starting CPSLocalFS install.")
-
+        
+        # Test if CPSLocalFS has all it needs.
+        if not self.testVersionLocalFS():
+            self.log("'LocalFS-1.2-andreas' not found")
+            self.log("CPSLocalFS install aborted")
+            return 0
+        
+        if not self.testReachConfigFile():
+            self.log("'localfs_dirs.txt' not found in HOME_ZOPE/var/")
+            self.log("CPSLocalFS install aborted")
+            return 0
+            
         # Portal CPS Update.
         self.updateCPS()
         
         # Portal Skins.
         self.verifySkins(self.SKINS)
         self.resetSkinCache()
-
-        # Portal Translation 
+        
+        # Portal Translation .
         self.setupTranslations('CPSLocalFS')
         
         # Portal Workflow.
@@ -64,8 +78,7 @@ class ProductInstaller(CPSInstaller):
                                         ws_chains)
         self.verifyLocalWorkflowChains(self.portal['sections'],
                                         se_chains)
-
-
+        
         # Portal Widgets.
         lfs_widgets = self.portal.getCPSLocalFSDocumentWidgets()
         self.verifyWidgets(lfs_widgets)
@@ -84,27 +97,22 @@ class ProductInstaller(CPSInstaller):
              },
         }
         self.verifyContentTypes(ptypes)
-
-   
-        
- 
         
         # Portal Schemas.
         lfs_schemas = self.portal.getCPSLocalFSDocumentSchemas()
         self.verifySchemas(lfs_schemas)
         #self.log(str(lfs_schemas))
-
-
-
-
+        
         # Portal Layouts.
         lfs_layouts = self.portal.getCPSLocalFSDocumentLayouts()
         self.verifyLayouts(lfs_layouts)
-        #self.log(str(lfs_layouts))
-
-
         
         # Portal Actions :
+        # call to updateActions removed, this method has been
+        # wrote as an exemple of how to remove the 'action_status_history'
+        # on CPSLocalFS but this should be done in the product using
+        # CPSLocalFS, not here.
+        
         #self.updateActions()
         
         self.log("End of CPSLocalFS install.")
@@ -116,18 +124,30 @@ class ProductInstaller(CPSInstaller):
         return self.portal.cpsupdate(self.portal)
     
     def updateActions(self):
-        """ Remove action_status_history, should not be done in \
+        """ Remove action_status_history, should not be done in 
         CPSLocalFS but in the product using it.
         """
         wftool = getToolByName(self.portal,'portal_actions')
         self.log("les actions du portail : ")
         for action in wftool._actions:
             if action.Title() == 'action_status_history':
-                action.condition = Expression("python:getattr(object, \
-                'portal_type', None) not in ('CPSLocalFS','Section', \
+                action.condition = Expression("python:getattr(object,\
+                'portal_type', None) not in ('CPSLocalFS','Section',\
                 'Workspace', 'Portal', 'Calendar', 'Event')")
-
-
+                
+    def testVersionLocalFS(self):
+        required_version ="LocalFS-1-2-andreas"
+        version_path = str(ZOPE_HOME) + "/Products/LocalFS/version.txt"
+        version_file= open(version_path)
+        installed_version = version_file.readline()
+        version_file.close()
+        return required_version==installed_version
+        
+       
+    def testReachConfigFile(self):
+        config_file_path = str(ZOPE_HOME) +"/var/localfs_dirs.txt"
+        return exists(config_file_path)
+        
 def install(self):
     installer = ProductInstaller(self)
     installer.install()
