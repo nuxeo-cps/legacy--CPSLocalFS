ea62e191a0caebdb832dac8f8ce05e14b478325d
sastier <devnull@localhost>
2004-04-16 10:28:15 +0000

    Add a test to check if the file '/CPSLocalFS/doc/install.txt' can be acceded via CPSLocalFS. If so CPSLocalFS and LocalFS are both working.

--- tests/testCPSLocalFS.py
+++ tests/testCPSLocalFS.py
@@ -6,7 +6,8 @@ import unittest
 from Testing import ZopeTestCase
 import CPSLocalFSTestCase
 from os.path import exists, isdir
-from zLOG import LOG, DEBUG, INFO
+from Products.CPSLocalFS.CPSLocalFS import CPSLocalFS
+
 
 class TestCPSLocalFS(CPSLocalFSTestCase.CPSLocalFSTestCase):
 
@@ -16,22 +17,23 @@ class TestCPSLocalFS(CPSLocalFSTestCase.CPSLocalFSTestCase):
     def beforeTearDown(self):
         self.logout()
 
-    def testVersionLocalFS(self):
-        required_version ="LocalFS-1-2-andreas"
-        version_path = str(ZOPE_HOME) + "/Products/LocalFS/version.txt"
-        version_file= open(version_path)
-        installed_version = version_file.readline()
-        version_file.close()
-        self.assert_(required_version==installed_version)
-        
-       
-    def testReachConfigFile(self):
-        config_file_path = str(ZOPE_HOME) +"/var/localfs_dirs.txt"
-        self.assert_(exists(config_file_path))
-
-
-        
-
+    def test_Access_CPSLocalFS_ok(self):
+        """ Make sure a file can be acceded """
+        path = ZOPE_HOME + "/Products/CPSLocalFS/doc"
+        title = "Test CPSLocalFS OK"
+        kw = {}
+        datamodel = {}
+        datamodel['basepath'] = path
+        datamodel['Title'] = title
+        kw['datamodel'] = datamodel
+        cpslocalfs = CPSLocalFS("sqfmdfj",**kw)
+        files = cpslocalfs.fileValues()
+        # Check if file 'install.txt' can be acceded
+        found = 0
+        for file in files:
+            if file.url == "install.txt":
+                found = 1
+        self.assert_(found)
 
 def test_suite():
     suite = unittest.TestSuite()
