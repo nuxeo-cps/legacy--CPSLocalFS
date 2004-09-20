2edf6aa95b2bb724c51c821fe37dc8c3ff87c536
edegoute <devnull@localhost>
2004-09-20 07:37:17 +0000

    installer cleaning

--- Extensions/install.py
+++ Extensions/install.py
@@ -46,7 +46,7 @@ class ProductInstaller(CPSInstaller):
     """ Installer for CPSLocalFS Product. """
     product_name = 'CPSLocalFS'
     SKINS = {'cps_localfs': 'Products/CPSLocalFS/skins/cps_localfs'}
-    
+
     def install(self):
         self.log("Starting CPSLocalFS install.")
 
@@ -55,29 +55,25 @@ class ProductInstaller(CPSInstaller):
             self.log("CPSLocalFS can only be installed on a linux server")
             self.log("CPSLocalFS install aborted")
         self.log("Operating System: " + platform[:5])
-    
-        
+
         # Test if CPSLocalFS has all it needs.
         if not self.testVersionLocalFS():
             self.log("LocalFS-1.2-andreas or LocalFS-1.3-andreas not found")
             self.log("CPSLocalFS install aborted")
             return 0
-        
+
         if not self.testReachConfigFile():
             self.log("'localfs_dirs.txt' not found in HOME_ZOPE/var/")
             self.log("CPSLocalFS install aborted")
             return 0
-            
-        # Portal CPS Update.
-        self.updateCPS()
-        
+
         # Portal Skins.
         self.verifySkins(self.SKINS)
         self.resetSkinCache()
-        
+
         # Portal Translation .
         self.setupTranslations('CPSLocalFS')
-        
+
         # Portal Workflow.
         self.allowContentTypes('CPSLocalFS', ('Workspace', 'Section'))
         ws_chains = {'CPSLocalFS': 'workspace_folder_wf',}
@@ -86,7 +82,7 @@ class ProductInstaller(CPSInstaller):
                                         ws_chains)
         self.verifyLocalWorkflowChains(self.portal['sections'],
                                         se_chains)
-        
+
         # Portal Widgets.
         lfs_widgets = self.portal.getCPSLocalFSDocumentWidgets()
         self.verifyWidgets(lfs_widgets)
@@ -105,12 +101,12 @@ class ProductInstaller(CPSInstaller):
              },
         }
         self.verifyContentTypes(ptypes)
-        
+
         # Portal Schemas.
         lfs_schemas = self.portal.getCPSLocalFSDocumentSchemas()
         self.verifySchemas(lfs_schemas)
         #self.log(str(lfs_schemas))
-        
+
         # Portal Layouts.
         lfs_layouts = self.portal.getCPSLocalFSDocumentLayouts()
         self.verifyLayouts(lfs_layouts)
@@ -120,17 +116,12 @@ class ProductInstaller(CPSInstaller):
         # wrote as an exemple of how to remove the 'action_status_history'
         # on CPSLocalFS but this should be done in the product using
         # CPSLocalFS, not here.
-        
+
         #self.updateActions()
-        
+
         self.log("End of CPSLocalFS install.")
         self.finalize()
-        
-    def updateCPS(self):
-        """Update CPS
-        """
-        return self.portal.cpsupdate(self.portal)
-    
+
     def updateActions(self):
         """ Remove action_status_history, should not be done in 
         CPSLocalFS but in the product using it.
@@ -142,7 +133,7 @@ class ProductInstaller(CPSInstaller):
                 action.condition = Expression("python:getattr(object,\
                 'portal_type', None) not in ('CPSLocalFS','Section',\
                 'Workspace', 'Portal', 'Calendar', 'Event')")
-                
+
     def testVersionLocalFS(self):
         required_version =('LocalFS-1-2-andreas', 'LocalFS-1-3-andreas')
         version_path = str(INSTANCE_HOME) + "/Products/LocalFS/version.txt"
@@ -163,6 +154,3 @@ def install(self):
     installer = ProductInstaller(self)
     installer.install()
     return installer.logResult()
-
-
-
